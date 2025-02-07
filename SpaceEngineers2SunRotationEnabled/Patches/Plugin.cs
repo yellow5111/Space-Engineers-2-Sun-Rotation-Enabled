c63322ac2ff244ebc57cbe98633c2d0691b6c564
yellow51 <57696915+yellow5111@users.noreply.github.com>
2025-02-06 20:30:19 -0700

    Init upld

---
+++ SpaceEngineers2SunRotationEnabled/Patches/Plugin.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using Keen.Game2.Simulation.GameSystems.Sun;
+using System.Reflection;
+
+namespace SpaceEngineers2EnableSunRotation
+{
+    [HarmonyPatch(typeof(SunSessionComponent), "Init")]
+    public class SunSessionComponent_EnableSunRotation
+    {
+        public static void Postfix(SunSessionComponent __instance)
+        {
+            var sunRotationField = typeof(SunSessionComponent).GetField("_sunRotation", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (sunRotationField != null)
+            {
+                
+                sunRotationField.SetValue(__instance, true);
+            }
+
+            MethodInfo sunDataChangedMethod = typeof(SunSessionComponent).GetMethod("SunDataChanged", BindingFlags.NonPublic | BindingFlags.Instance);
+            sunDataChangedMethod?.Invoke(__instance, null);
+        }
+    }
+}
---
+++ SpaceEngineers2SunRotationEnabled/Plugin.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using Keen.Game2.Game.Plugins;
+using Keen.VRage.Library.Diagnostics;
+using System.Reflection;
+
+namespace SpaceEngineers2NoPCU
+{
+    public class Plugin : IPlugin
+    {
+        public const string Name = "Space Engineers 2 - Sun rotation";
+
+        public Plugin()
+        {
+            Log.Default.WriteLine($"[{Name}] loaded...");
+#if DEBUG
+            Harmony.DEBUG = true;
+#endif
+            Harmony harmony = new Harmony(Name);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            Log.Default.WriteLine($"[{Name}] applied...");
+        }
+    }
+}
